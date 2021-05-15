f9bf1f3363b549f0b160a99db8da3370a7917577
xraybrain <xraybrain@gmail.com>
2021-05-15 08:30:03 +0100

    fixed login bug

--- login.php
+++ login.php
@@ -8,9 +8,9 @@ require "db.php";
 //Function to sanitize values received from the form. Prevents SQL injection
 function clean($str, $bd) {
 	$str = @trim($str);
-	if(get_magic_quotes_gpc()) {
-		$str = stripslashes($str);
-	}
+	// if(get_magic_quotes_gpc()) {
+	// 	$str = stripslashes($str);
+	// }
 	return mysqli_real_escape_string($bd,$str);
 }
 
