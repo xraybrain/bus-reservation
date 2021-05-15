60cff50739c352e6a93784b8bf278682ddea1ed1
xraybrain <xraybrain@gmail.com>
2021-05-15 08:16:47 +0100

    update: configured db

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
-$mysql_database = "ticket";
+$mysql_hostname = "us-cdbr-east-03.cleardb.com";
+$mysql_user = "b6a2811383209b";
+$mysql_password = "bf9c01f5";
+$mysql_database = "heroku_ccf9ece95dea209";
 $prefix = "";
 $bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
 mysqli_select_db($bd, $mysql_database) or die("Could not select database");
